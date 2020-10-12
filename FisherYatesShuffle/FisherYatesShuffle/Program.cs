2f9f0bbe70e2583b51d9115a075bed60e4b3c4e5
Charlie Phillips <hi.charliephillips@gmail.com>
2020-10-12 15:39:33 -0500

    added shuffle logic

--- FisherYatesShuffle/FisherYatesShuffle/Program.cs
+++ FisherYatesShuffle/FisherYatesShuffle/Program.cs
@@ -6,7 +6,16 @@ namespace FisherYatesShuffle
     {
         static void Main(string[] args)
         {
-            Shuffle shuffle = new Shuffle();
+            //create example array
+            string[] letters = { "B", "A", "C" };
+
+            Shuffle.DoShuffle(letters); //call shuffle
+
+            //display shuffled results
+            foreach(string letter in letters)
+                Console.Write(letter + " ");
+
+            Console.ReadLine();
         }
     }
 }
--- FisherYatesShuffle/FisherYatesShuffle/Shuffle.cs
+++ FisherYatesShuffle/FisherYatesShuffle/Shuffle.cs
@@ -1,18 +1,30 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace FisherYatesShuffle
 {
-    class Shuffle
+    public static class Shuffle
     {
+        private static Random rand = new Random();
 
-
-        public Shuffle()
+        public static void DoShuffle(this object[] objects)
         {
-
+            for (int i = objects.Length - 1; i > 0; i--)
+            {
+                Swap(objects, i, GetRandomNumber(i));
+            }
         }
 
+        private static void Swap(object[] objects, int i, int j)  //swap i and j
+        {
+            object temp = objects[i];
+            objects[i] = objects[j];
+            objects[j] = temp;
+        }
+
+        private static int GetRandomNumber(int i)
+        {
+            return rand.Next(i + 1);
+        }
 
     }
 }
