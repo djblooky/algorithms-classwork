f86b3e76cba2975cd6f91109b2b07f69302145b9
Charlie Phillips <hi.charliephillips@gmail.com>
2020-12-14 18:36:15 -0600

    Create Merge.cs

---
+++ Sorting/Merge.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sorting
+{
+    public static class Merge
+    {
+        public static int[] Sort(int[] data)
+        {
+
+
+
+            return data;
+        }
+    }
+}
